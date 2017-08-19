6ab212cdce6a2fd961d5b135c7957679e316ca39
LBJ <LBJ>
2017-08-19 12:15:53 +0800

    20170819

--- app/src/main/java/tw/ming/app/helloworid/myasynctask/MainActivity.java
+++ app/src/main/java/tw/ming/app/helloworid/myasynctask/MainActivity.java
@@ -1,6 +1,11 @@
 package tw.ming.app.helloworid.myasynctask;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.AsyncTask;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,72 +14,95 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private MyAsyncTask myAsyncTask;
-    private TextView tv;
+    private TextView tv, tv2;
+    private MyServiceConnection myServiceConnection;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView)findViewById(R.id.tv);
+        tv2 = (TextView)findViewById(R.id.tv2);
+
+        Intent it = new Intent(this, MyService1.class);
+        myServiceConnection = new MyServiceConnection();
+        bindService(it,myServiceConnection, Context.BIND_AUTO_CREATE);
+
     }
 
-    public void test1(View view){
-        myAsyncTask = new MyAsyncTask();
-        myAsyncTask.execute("ming","iii");
-    }
+    private class MyServiceConnection implements ServiceConnection{
 
-    public void test2(View view){
-        tv.setText("Lottery:"+(int)(Math.random()*49+1));
-    }
-    private  class MyAsyncTask extends AsyncTask<String,Void,Void>{
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
 
-        public MyAsyncTask() {
-            super();
         }
 
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    }
+    public void test1(View view){
+        myAsyncTask = new MyAsyncTask();
+        myAsyncTask.execute("Ming", "III", "OK", "Game", "IOS");
+    }
+    public void test2(View view){//部會干擾test1執行
+        tv.setText("Lottery: " + (int)(Math.random()*49+1));
+    }
+    public void test3(View view){
+        if (myAsyncTask!=null && !myAsyncTask.isCancelled()){
+            myAsyncTask.cancel(true);//強制中斷
+        }
+    }
+
+    private class MyAsyncTask extends AsyncTask<String,Integer,String>{
+        int i;
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            Log.i("ming","onPreExecute");
+            Log.i("ming", "onPreExecute");
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            Log.i("ming","onPostExecute");
+        protected void onPostExecute(String ret) {
+            super.onPostExecute(ret);
+            tv.setText(ret);
         }
 
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
-            for(Integer j:values){
-                tv.setText(values[0]+":"+values[1]+":"+values[2]);
-            }
+            tv2.setText(values[0] + " : " + values[1] + " : " + values[2]);
         }
 
         @Override
-        protected void onCancelled(Void aVoid) {
-            super.onCancelled(aVoid);
+        protected void onCancelled(String result) {
+            super.onCancelled(result);
+            tv.setText(result);
         }
 
         @Override
         protected void onCancelled() {
             super.onCancelled();
+            Log.i("ming", "onCancelled");
         }
 
         @Override
-        protected Void doInBackground(String... names) {
-            for(String name:names){
-                Log.i("ming",name);
-                publishProgress(i,i*10,i*100);
+        protected String doInBackground(String... names) {
+            String result = "OK";
+            for (String name : names){
+                i++;
+                Log.i("ming", name);
+                publishProgress(i, i*10, i*100);//執行時一起加入
+                if (isCancelled()){
+                    result = "NOT OK";
+                    break;
+                }
                 try {
                     Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                }catch (InterruptedException e){}
             }
-
-
-            return null;
+            return result;
         }
     }
+
 }
---
+++ app/src/main/java/tw/ming/app/helloworid/myasynctask/MyService1.java
@@ -0,0 +1,40 @@
+package tw.ming.app.helloworid.myasynctask;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Created by Mac on 2017/8/19.
+ */
+
+public class MyService1 extends Service{
+    public MyService1() {
+        Log.i("ming", "MySrvice1()");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i("ming", "onBind");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i("ming", "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("ming", "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("ming", "onDestroy");
+    }
+}
